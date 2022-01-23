cc9bef0505a1fe3bf0132c7b1ba97d3854883520
JoseAnnunziato <jose.annunziato@sonos.com>
2022-01-23 15:42:09 -0500

    adding controller

--- controllers/CourseController.ts
+++ controllers/CourseController.ts
@@ -3,58 +3,57 @@ import {Express, Request, Response} from "express";
 import CourseDao from "../daos/CourseDao";
 
 export default class CourseController implements CourseControllerI {
-    private courseDao = CourseDao.getInstance();
     constructor(app: Express) {
         app.get("/courses", this.findAllCourses);
-        // app.get("/courses/:cid", this.findCourseById);
-        // app.post("/courses", this.createCourse);
-        // app.put("/courses/:cid", this.updateCourse);
-        // app.delete("/courses/:cid", this.deleteCourse);
+        app.get("/courses/:cid", this.findCourseById);
+        app.post("/courses", this.createCourse);
+        app.put("/courses/:cid", this.updateCourse);
+        app.delete("/courses/:cid", this.deleteCourse);
     }
     findAllCourses(req: Request, res: Response): void {
-        this.courseDao.findAllCourses()
+        CourseDao.getInstance().findAllCourses()
             .then(courses => res.json(courses));
     }
-    // findAllCoursesDeep(req: Request, res: Response): void {
-    //     this.courseDao.findAllCoursesDeep()
-    //         .then(courses => res.json(courses));
-    // }
-    // findCourseById(req: Request, res: Response): void {
-    //     const cid = req.params.cid;
-    //     this.courseDao.findCourseById(cid)
-    //         .then(course => res.json(course));
-    // }
-    // findCourseByIdDeep(req: Request, res: Response): void {
-    //     const cid = req.params.cid;
-    //     this.courseDao.findCourseByIdDeep(cid)
-    //         .then(course => res.json(course));
-    // }
-    // createCourse(req: Request, res: Response): void {
-    //     const course = req.body;
-    //     this.courseDao.createCourse(course)
-    //         .then(course => res.json(course));
-    // }
-    // deleteCourse(req: Request, res: Response): void {
-    //     const cid = req.params.cid;
-    //     this.courseDao.deleteCourse(cid)
-    //         .then(status => res.send(status));
-    // }
-    // updateCourse(req: Request, res: Response): void {
-    //     const cid = req.params.cid;
-    //     const course = req.body;
-    //     this.courseDao.updateCourse(cid, course)
-    //         .then(status => res.send(status));
-    // }
-    // addSectionToCourse(req: Request, res: Response): void {
-    //     const cid = req.params.cid;
-    //     const sid = req.params.sid;
-    //     this.courseDao.addSectionToCourse(cid, sid)
-    //         .then(status => res.send(status));
-    // }
-    // removeSectionFromCourse(req: Request, res: Response): void {
-    //     const cid = req.params.cid;
-    //     const sid = req.params.sid;
-    //     this.courseDao.removeSectionFromCourse(cid, sid)
-    //         .then(status => res.send(status));
-    // }
+    findAllCoursesDeep(req: Request, res: Response): void {
+        CourseDao.getInstance().findAllCoursesDeep()
+            .then(courses => res.json(courses));
+    }
+    findCourseById(req: Request, res: Response): void {
+        const cid = req.params.cid;
+        CourseDao.getInstance().findCourseById(cid)
+            .then(course => res.json(course));
+    }
+    findCourseByIdDeep(req: Request, res: Response): void {
+        const cid = req.params.cid;
+        CourseDao.getInstance().findCourseByIdDeep(cid)
+            .then(course => res.json(course));
+    }
+    createCourse(req: Request, res: Response): void {
+        const course = req.body;
+        CourseDao.getInstance().createCourse(course)
+            .then(course => res.json(course));
+    }
+    deleteCourse(req: Request, res: Response): void {
+        const cid = req.params.cid;
+        CourseDao.getInstance().deleteCourse(cid)
+            .then(status => res.send(status));
+    }
+    updateCourse(req: Request, res: Response): void {
+        const cid = req.params.cid;
+        const course = req.body;
+        CourseDao.getInstance().updateCourse(cid, course)
+            .then(status => res.send(status));
+    }
+    addSectionToCourse(req: Request, res: Response): void {
+        const cid = req.params.cid;
+        const sid = req.params.sid;
+        CourseDao.getInstance().addSectionToCourse(cid, sid)
+            .then(status => res.send(status));
+    }
+    removeSectionFromCourse(req: Request, res: Response): void {
+        const cid = req.params.cid;
+        const sid = req.params.sid;
+        CourseDao.getInstance().removeSectionFromCourse(cid, sid)
+            .then(status => res.send(status));
+    }
 }
--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
-// import CourseController from "./controllers/CourseController";
+import CourseController from "./controllers/CourseController";
+import mongoose from "mongoose";
 const app = express();
+mongoose.connect('mongodb://localhost:27017/cs5500-test-123');
 
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
@@ -9,7 +11,7 @@ app.get('/add/:a/:b', (req, res) => {
     res.send(req.params.a + req.params.b);
 })
 
-// const courseController = new CourseController(app);
+const courseController = new CourseController(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
