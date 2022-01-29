c9b1fced6f9b4296237642ec172df25a9e4c471a
jannunzi <jannunzi@gmail.com>
2022-01-29 17:55:49 -0500

    user controller

---
+++ controllers/UserController.ts
@@ -0,0 +1,47 @@
+import UserDao from "../daos/UserDao";
+import User from "../models/users/User";
+import {Express, Request, Response} from "express";
+import UserControllerI from "../interfaces/UserControllerI";
+
+export default class UserController implements UserControllerI {
+    private static userDao: UserDao = UserDao.getInstance();
+    private static userController: UserController | null = null;
+    public static getInstance = (app: Express): UserController => {
+        if(UserController.userController === null) {
+            UserController.userController = new UserController();
+            app.get("/api/hello", (req, res) => res.send('hello from users'));
+            app.get("/api/users", UserController.userController.findAllUsers);
+            app.get("/api/users/:uid", UserController.userController.findUserById);
+            app.post("/api/users", UserController.userController.createUser);
+            app.put("/api/users/:uid", UserController.userController.updateUser);
+            app.delete("/api/users/:uid", UserController.userController.deleteUser);
+        }
+        return UserController.userController;
+    }
+
+    private constructor() {}
+
+    // constructor(app: Express) {
+    //     app.get("/api/hello", (req, res) => res.send('hello from users'));
+    //     app.get("/api/users", this.findAllUsers);
+    //     app.get("/api/users/:uid", this.findAllUsers);
+    //     app.post("/api/users", this.createUser);
+    //     app.put("/api/users/:uid", this.updateUser);
+    //     app.delete("/api/users/:uid", this.deleteUser);
+    // }
+    findAllUsers = (req: Request, res: Response) =>
+        UserController.userDao.findAllUsers()
+            .then((users: User[]) => res.json(users));
+    findUserById = (req: Request, res: Response) =>
+        UserController.userDao.findUserById(req.params.uid)
+            .then((user: User) => res.json(user));
+    createUser = (req: Request, res: Response) =>
+        UserController.userDao.createUser(req.body)
+            .then((user: User) => res.json(user));
+    updateUser = (req: Request, res: Response) =>
+        UserController.userDao.updateUser(req.params.uid, req.body)
+            .then((status) => res.send(status));
+    deleteUser = (req: Request, res: Response) =>
+        UserController.userDao.deleteUser(req.params.uid)
+            .then((status) => res.send(status));
+};
---
+++ interfaces/UserControllerI.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from "express";
+import User from "../models/users/User";
+
+export default interface UserControllerI {
+    findAllUsers (req: Request, res: Response): void;
+    findUserById (req: Request, res: Response): void;
+    createUser (req: Request, res: Response): void;
+    updateUser (req: Request, res: Response): void;
+    deleteUser (req: Request, res: Response): void;
+};
