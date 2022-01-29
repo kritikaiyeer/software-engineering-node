e4a92f798454a9dd23d7dcf8f2d2dc9e0b21afb2
jannunzi <jannunzi@gmail.com>
2022-01-29 17:56:27 -0500

    user REST API

--- server.ts
+++ server.ts
@@ -2,10 +2,13 @@
  * @file Server file
  */
 import express, {Request, Response} from 'express';
-import CourseController from "./controllers/CourseController";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import CourseController from "./controllers/CourseController";
+import UserController from "./controllers/UserController";
 const app = express();
 mongoose.connect('mongodb://localhost:27017/cs5500-test-123');
+app.use(bodyParser.json())
 
 app.get('/', (req: Request, res: Response) =>
     res.send('Welcome!'));
@@ -14,6 +17,7 @@ app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
 
 const courseController = new CourseController(app);
+const userController = UserController.getInstance(app);
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
