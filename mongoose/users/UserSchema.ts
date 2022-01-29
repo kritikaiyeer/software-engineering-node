106704d4a7a38eedebf89ddd79faee6fdf56ea7d
jannunzi <jannunzi@gmail.com>
2022-01-29 17:55:16 -0500

    user mongoose schema and model

---
+++ mongoose/users/UserSchema.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import User from "../../models/users/User";
+const UserSchema = new mongoose.Schema<User>({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    firstName: String,
+    lastName: String,
+    email: {type: String, required: true},
+    profilePhoto: String,
+    headerImage: String,
+    biography: String,
+    dateOfBirth: Date,
+    accountType: {type: String, enum: ["PERSONAL", "ACADEMIC", "PROFESSIONAL"]},
+    maritalStatus: {type: String, enum: ["MARRIED", "SINGLE", "WIDOWED"]},
+    location: {
+        latitude: Number,
+        longitude: Number
+    },
+}, {collection: "users"});
+
+export default UserSchema;
