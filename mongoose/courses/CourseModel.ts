8c0823aad2f129bf73985c1b79844e2742f0e691
jannunzi <jannunzi@gmail.com>
2022-01-29 17:50:38 -0500

    removed unnecessary comments

--- mongoose/courses/CourseModel.ts
+++ mongoose/courses/CourseModel.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 import CourseSchema from "./CourseSchema";
-// import Course from "../../models/Course";
 import Course from "./Course";
 const CourseModel = mongoose.model<Course>(
     "CourseModel",
     CourseSchema
 );
-// CourseModel.createCollection({})
 export default CourseModel;
