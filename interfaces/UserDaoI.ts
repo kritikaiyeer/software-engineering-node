ee9e0d110ab59b109e1e94173773e6e8bfd77b83
jannunzi <jannunzi@gmail.com>
2022-01-29 17:55:38 -0500

    user data model

---
+++ interfaces/UserDaoI.ts
@@ -0,0 +1,12 @@
+import User from "../models/users/User";
+
+/**
+ * @file Declares API for User related data access object methods
+ */
+export default interface UserDaoI {
+    findAllUsers (): Promise<User[]>;
+    findUserById (uid: string): Promise<any>;
+    createUser (user: User): Promise<User>;
+    updateUser (uid: string, user: User): Promise<any>;
+    deleteUser (uid: string): Promise<any>;
+};
