37309ad29b7e0a098a436e2f24e47ce89693eebb
jannunzi <jannunzi@gmail.com>
2022-01-29 19:27:09 -0500

    tuits dao

---
+++ daos/TuitDao.ts
@@ -0,0 +1,29 @@
+import TuitModel from "../mongoose/tuits/TuitModel";
+import Tuit from "../models/tuits/Tuit";
+import TuitDaoI from "../interfaces/TuitDaoI";
+export default class TuitDao implements TuitDaoI{
+    private static tuitDao: TuitDao | null = null;
+    public static getInstance = (): TuitDao => {
+        if(TuitDao.tuitDao === null) {
+            TuitDao.tuitDao = new TuitDao();
+        }
+        return TuitDao.tuitDao;
+    }
+    private constructor() {}
+    findAllTuits = async (): Promise<Tuit[]> =>
+        TuitModel.find();
+    findAllTuitsByUser = async (uid: string): Promise<Tuit[]> =>
+        TuitModel.find({postedBy: uid});
+    findTuitById = async (uid: string): Promise<any> =>
+        TuitModel.findById(uid)
+            .populate("postedBy")
+            .exec();
+    createTuitByUser = async (uid: string, tuit: Tuit): Promise<Tuit> =>
+        TuitModel.create({...tuit, postedBy: uid});
+    updateTuit = async (uid: string, tuit: Tuit): Promise<any> =>
+        TuitModel.updateOne(
+            {_id: uid},
+            {$set: tuit});
+    deleteTuit = async (uid: string): Promise<any> =>
+        TuitModel.deleteOne({_id: uid});
+}
