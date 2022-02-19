9afac1081dd2a60163ee2d1994c6c651d664b661
jannunzi <jannunzi@gmail.com>
2022-02-19 18:28:52 -0500

    integrating with ui

--- mongoose/tuits/TuitSchema.ts
+++ mongoose/tuits/TuitSchema.ts
@@ -3,6 +3,15 @@ import Tuit from "../../models/tuits/Tuit";
 const TuitSchema = new mongoose.Schema<Tuit>({
     tuit: {type: String, required: true},
     postedBy: {type: Schema.Types.ObjectId, ref: "UserModel"},
-    postedOn: {type: Date, default: Date.now}
+    postedOn: {type: Date, default: Date.now},
+    image: String,
+    youtube: String,
+    avatarLogo: String,
+    imageOverlay: String,
+    stats: {
+        replies: Number,
+        retuits: Number,
+        likes: Number
+    }
 }, {collection: "tuits"});
 export default TuitSchema;
